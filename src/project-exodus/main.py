ecd68df6cd6206fbbf91b38aa3acff91c85e2fb3
Space Nerd <florianpospiech77@gmail.com>
2023-03-30 20:30:51 +0200

    add basic structur

--- src/project-exodus/main.py
+++ src/project-exodus/main.py
@@ -1,1 +1,15 @@
-#! 
+# Project Exodus
+# Created 30.03.2023 20:28
+#
+# Author: SpaceNerde
+
+if __name__ == "__main__":
+    print("running...")
+
+    # Code -start
+
+
+
+    # Code -end
+
+    print("stopped")
